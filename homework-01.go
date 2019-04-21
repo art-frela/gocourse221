3fdb6c3484a34f70b5501c3f349a64fa62752ba2
Artem Karpov from Ubuntu <art.frela@gmail.com>
2019-04-21 16:02:04 +0300

    fix some work at the village

--- homework-01.go
+++ homework-01.go
@@ -10,6 +10,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
 )
@@ -25,16 +26,18 @@ func main() {
 
 	var greeting string
 
-	greetingReset(&greeting)
+	greetingSet(&greeting, 0, usdRate)
 	//loop for continuous program execution
 	for {
 		fmt.Print(greeting)
+		log.Println("Programmode=", programMode)
 		fmt.Scanln(&input)
 		if strings.ToUpper(input) == "Q" {
 			fmt.Println("Good buy!")
 			return
 		}
 		if strings.ToUpper(input) == "H" {
+			greetingSet(&greeting, 0, usdRate)
 			programMode = 0
 		}
 
@@ -43,20 +46,30 @@ func main() {
 			switch input {
 			case "1":
 				programMode = 1
+				greetingSet(&greeting, 1, usdRate)
+				continue
 			case "2":
 				programMode = 2
+				greetingSet(&greeting, 2, usdRate)
+				continue
 			case "3":
 				programMode = 3
+				greetingSet(&greeting, 3, usdRate)
+				continue
+			default:
+				fmt.Println("Wrong choice <" + input + ">, try again!")
+				continue
 			}
 		}
 
-		switch programMode {
-		case 0:
-			greetingReset(&greeting)
-			continue
-		case 1:
-
-		}
+		// switch programMode {
+		// case 0:
+		// 	greetingReset(&greeting)
+		// 	continue
+		// case 1:
+		// 	greetingSet(1)
+		// 	fmt.Println("")
+		// }
 		greeting := "<" + input + "> (main menu (h), exit (q))>"
 		switch input {
 		case "1":
@@ -76,13 +89,24 @@ func main() {
 
 }
 
-func greetingReset(greeting *string) {
-	*greeting = "You can choose which feature do you need:\n1-currency converter\n2-calculator for right triangle\n3-bank deposite calculator\n"
-	*greeting += "Type some number 1, 2, 3 and press Enter. Main menu (h), for exit (q): "
+func greetingSet(greeting *string, opt int, usdrate float64) {
+	switch opt {
+	case 0:
+		*greeting = "You can choose which feature do you need:\n1-currency converter\n2-calculator for right triangle\n3-bank deposite calculator\n"
+		*greeting += "Type some number 1, 2, 3 and press Enter. Main menu (h), for exit (q): "
+	case 1:
+		*greeting = "<1> (main menu (h), exit (q))>Currency calculartor. USD Rate =" + strconv.FormatFloat(usdrate, 'f', 2, 32) + "rub/1usd. Type how much rubles do you want convert to USD?:"
+	case 2:
+		*greeting = "You can choose which feature do you need:\n1-currency converter\n2-calculator for right triangle\n3-bank deposite calculator\n"
+		*greeting += "Type some number 1, 2, 3 and press Enter. Main menu (h), for exit (q): "
+	case 3:
+		*greeting = "You can choose which feature do you need:\n1-currency converter\n2-calculator for right triangle\n3-bank deposite calculator\n"
+		*greeting += "Type some number 1, 2, 3 and press Enter. Main menu (h), for exit (q): "
+	}
 }
 
 //currencyConv - currency converter (rubles to usd dollars)
 func currencyConv(usdRate, rubles float64) string {
 	//only in one direction converter
-	return strconv.FormatFloat(rubles/usdRate, 'f', 3, 32)
+	return strconv.FormatFloat(rubles/usdRate, 'f', 2, 32)
 }
