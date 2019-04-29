3a3e732a510978c2272a55f314e9b39a445fb8ec
Artem K <art.frela@gmail.com>
2019-04-29 17:26:36 +0300

    add comments and implement tasks
    implemet tasks 3 and 4

--- homework-03/homework-03.go
+++ homework-03/homework-03.go
@@ -10,9 +10,12 @@ Date: 2019-04-29
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 )
 
+//TrackCar - structure for Track car
 type TrackCar struct {
 	VIN             string  `json:"vin"`
 	Mark            string  `json:"mark"`
@@ -24,6 +27,7 @@ type TrackCar struct {
 	BodyOccupancy   float64 `json:"bodyoccupancy"`
 }
 
+//PassageCar - structure for passage car
 type PassageCar struct {
 	VIN               string  `json:"vin"`
 	Mark              string  `json:"mark"`
@@ -35,12 +39,18 @@ type PassageCar struct {
 	TrunkOccupancy    float64 `json:"trunkoccupancy"`
 }
 
+//Message - message structure
+type Message struct {
+	ID   int    `json:"id"`
+	Body string `json:"body"`
+}
+
 func main() {
 
-	//task - 1
-	//Описать несколько структур — любой легковой автомобиль и грузовик.
-	//Структуры должны содержать марку авто, год выпуска, объем багажника/кузова,
-	//запущен ли двигатель, открыты ли окна, насколько заполнен объем багажника.
+	// [TASK-1]
+	// Описать несколько структур — любой легковой автомобиль и грузовик.
+	// Структуры должны содержать марку авто, год выпуска, объем багажника/кузова,
+	// запущен ли двигатель, открыты ли окна, насколько заполнен объем багажника.
 	car1 := PassageCar{
 		VIN:               "536DFS@$3465657",
 		Mark:              "Skoda",
@@ -65,27 +75,126 @@ func main() {
 
 	printSome("After Initialazed: ", car1, truck1)
 
-	//task 2
-	//Инициализировать несколько экземпляров структур. Применить к ним различные действия.
-	//Вывести значения свойств экземпляров в консоль.
+	// [TASK-2]
+	// Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+	// Вывести значения свойств экземпляров в консоль.
 
-	//to Run cars
+	// to Run cars
 	car1.EngineIsRun = true
 	truck1.EngineIsRun = true
 	printSome("Engine run: ", car1, truck1)
 
-	//to open windows
+	// to open windows
 	car1.WindowIsOpen = true
 	truck1.WindowIsOpen = true
 	printSome("Windows open: ", car1, truck1)
-	//to fill 45% truck1
+	// to fill 45% truck1
 	truck1.BodyOccupancy = 45.4
 	printSome("to fill 45% truck1: ", car1, truck1)
 
+	// [TASK-3]
+	// * Реализовать очередь. Это структура данных, работающая по принципу FIFO
+	// (First Input — first output, или «первым зашел — первым вышел»).
+	printSome("\n\n\nTASK-3:")
+	var queueOfMessages = make([]Message, 0, 20)
+
+	// simple push message to queue
+	examplePushOrderedMessage(5, &queueOfMessages)
+
+	printSome("Basic queue: ", queueOfMessages)
+
+	reverseQueue := reverseQueueMessage(queueOfMessages)
+	for ix, msg := range reverseQueue {
+		fmt.Printf("Index=%d, Message=%+v\n", ix, msg)
+	}
+
+	printSome("\n\n\nTASK-4:")
+	// [TASK-4]
+	// * Внести в телефонный справочник дополнительные данные.
+	// Реализовать сохранение json-файла на диске с помощью пакета ioutil при изменении данных.
+
+	addressBook := make(map[string][]int)
+
+	addressBook["Alex"] = []int{89996543210}
+	addressBook["zoro"] = []int{89030011234}
+	addressBook["Bob"] = []int{89167243812}
+	saveAddressbook("addressbook.json", true, addressBook)
+	addressBookBefore := addressBook
+	addressBook["Bob"] = append(addressBook["Bob"], 89155243627)
+	if mapEq(addressBookBefore, addressBook) {
+		saveAddressbook("addressbook.json", true, addressBook)
+	}
+
+	fmt.Println("Done.")
 }
 
-func printSome(structurs ...interface{}) {
-	for _, val := range structurs {
+// printSome - simple analog of fmt.Println but in a column
+func printSome(args ...interface{}) {
+	for _, val := range args {
 		fmt.Printf("%+v\n", val)
 	}
+}
+
+// examplePushOrderedMessage - simple filler for slice of Messages
+func examplePushOrderedMessage(count int, queue *[]Message) {
+	for i := 1; i <= count; i++ {
+		tmpMsg := Message{
+			i,
+			fmt.Sprintf("Message # %d", i),
+		}
+		*queue = append(*queue, tmpMsg)
+	}
+}
+
+// reverseQueueMessage - make reverse slice, input = Message slice, output = reverse Message slice
+func reverseQueueMessage(inputQ []Message) (output []Message) {
+	maxIxInput := len(inputQ) - 1
+	for ix := maxIxInput; ix >= 0; ix-- {
+		output = append(output, inputQ[ix])
+	}
+	return
+}
+
+// equal - compare maps, if equal return true
+func mapEq(left, right map[string][]int) bool {
+	if len(left) != len(right) {
+		return false
+	}
+
+	for lix, lval := range left {
+		if rval, ok := right[lix]; !ok || !sliceEq(rval, lval) {
+			return false
+		}
+	}
+	return true
+}
+
+// sliceEq - compares two slices, if eq return true
+func sliceEq(left, right []int) bool {
+	if len(left) != len(right) {
+		return false
+	}
+
+	for lix, lval := range left {
+		if rval := right[lix]; lval != rval {
+			return false
+		}
+	}
+	return true
+}
+
+// saveAddressbook - saves the addressbook to dist, json format
+func saveAddressbook(filename string, pretty bool, data map[string][]int) (err error) {
+	var dataSave []byte
+	if pretty {
+		dataSave, err = json.MarshalIndent(data, "", "\t")
+	} else {
+		dataSave, err = json.Marshal(data)
+	}
+	if err != nil {
+		return
+	}
+	//save to disk
+	err = ioutil.WriteFile(filename, dataSave, 0644)
+	return
 }
