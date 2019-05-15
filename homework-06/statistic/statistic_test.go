eba5cf4329932b080eb027fbfc44bd817a58c52e
Artem Karpov from Ubuntu <art.frela@gmail.com>
2019-05-16 00:00:11 +0300

    implement task#4
    very useful execise

--- homework-06/statistic/statistic_test.go
+++ homework-06/statistic/statistic_test.go
@@ -1,6 +1,9 @@
 package statistic
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAverage(t *testing.T) {
 	var v float64
@@ -15,6 +18,12 @@ type testpair struct {
 	result float64
 }
 
+type testpairSQR struct {
+	values [3]float64
+	result []float64
+	err    error
+}
+
 var testsAVG = []testpair{
 	{[]float64{1, 2}, 1.5},
 	{[]float64{1, 1, 1, 1, 1, 1}, 1},
@@ -25,6 +34,13 @@ var testsSUM = []testpair{
 	{[]float64{-100, 200, 50, -150, 5, 6, 9}, 20},
 	{[]float64{-100000, 50000, 25000, -12500, 45321, 3456, 3235, -12456, -342, -1014}, 700},
 	{[]float64{0, 0, 0, -0, +1, 2, 5}, 8},
+}
+
+var testSQREqu = []testpairSQR{
+	{[3]float64{float64(-12), float64(5), float64(43)}, []float64{-1.6960658136710733, 2.11273248033774}, nil},
+	{[3]float64{float64(-77), float64(-77), float64(-77)}, []float64{}, fmt.Errorf("no roots")},
+	{[3]float64{float64(800), float64(-77000), float64(0)}, []float64{float64(96.25), float64(0)}, nil},
+	{[3]float64{float64(0), float64(1), float64(99)}, []float64{float64(-99)}, nil},
 }
 
 // TestAverageSet - test func for Average()
@@ -51,6 +67,16 @@ func TestSummaSet(t *testing.T) {
 				"expected", pair.result,
 				"got", v,
 			)
+		}
+	}
+}
+
+// TestSqrEquation -
+func TestSqrEquation(t *testing.T) {
+	for i := 0; i < len(testSQREqu); i++ {
+		roots, terr := SqrEquation(testSQREqu[i].values)
+		if (terr == nil && testSQREqu[i].err != nil) || (terr != nil && testSQREqu[i].err == nil) || !compareSliceFloat64(roots, testSQREqu[i].result) {
+			t.Errorf("For %v expected %v, but got %v", testSQREqu[i].values, testSQREqu[i].result, roots)
 		}
 	}
 }
